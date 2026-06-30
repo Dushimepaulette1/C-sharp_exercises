6d282559bf685dc265a0d4c09631f364667ecbb9
Dushimepaulette1 <p.dushime1@alustudent.com>
2026-06-30 10:50:24 +0200

    manipulating strings using ToLower() and ToUpper()

--- Strings.cs
+++ Strings.cs
@@ -68,8 +68,32 @@ public class Strings
 
         // DNA mutation search
         Console.WriteLine(dna[3]);
+        
+        
+        //Manipulating strings in C#
+        // Script line
+        string script = "Close on a portrait of the HANDSOME PRINCE -- as the BEAST'S giant paw slashes it.";
+
+        // Get camera directions
+        int charPosition = script.IndexOf("Close");
+        int length = "Close on".Length;
+        string cameraDirections = script.Substring(charPosition, length);
+
+        // Get scene description
+        charPosition = script.IndexOf("a portrait");
+        string sceneDescription = script.Substring(charPosition);
+
+        // Make camera directions uppercase
+        cameraDirections = cameraDirections.ToUpper();
 
 
+        // Make scene description lowercase
+        sceneDescription = sceneDescription.ToLower();
+
+
+        // Print results
+        Console.WriteLine(cameraDirections);
+        Console.WriteLine(sceneDescription);
 
     }
 }
