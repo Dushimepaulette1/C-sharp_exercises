5aa6d6d3fe8487b27fb84d285c8e29aae1616c19
Dushimepaulette1 <p.dushime1@alustudent.com>
2026-06-30 15:47:13 +0200

    else if conditionals when we have many conditions

--- Conditionals.cs
+++ Conditionals.cs
@@ -2,14 +2,30 @@ public class Conditionals
 {
     public static void ConditionalsRun()
     {
-        int socks = 6;
-        if(socks <= 3)
+        // int socks = 6;
+        // if(socks <= 3)
+        // {
+        //     Console.WriteLine("Time to do laundry!");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Time to do lost!");
+        // }
+        
+        //using the ph example to show how else if conditionals work
+        
+        double ph = 7;
+        if(ph < 7)
         {
-            Console.WriteLine("Time to do laundry!");
+            Console.WriteLine("Acidic");
+        }
+        else if(ph > 7)
+        {
+            Console.WriteLine("Basic");
         }
         else
         {
-            Console.WriteLine("Time to do lost!");
+            Console.WriteLine("Neutral");
         }
     }
 }
