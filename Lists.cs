b339e621974b0eaed026bf51efd0992eea453ac2
Dushimepaulette1 <p.dushime1@alustudent.com>
2026-07-01 16:22:30 +0200

    creating lists and also adding in them

---
+++ Lists.cs
@@ -0,0 +1,13 @@
+public class Lists
+{
+    public static void ListsRun()
+    {
+        //Declaring a list of doubles 
+        List<double> marathons = new List<double> ();
+        //Adding to the list 
+        marathons.Add(144.07);
+        marathons.Add(143.12);
+        Console.WriteLine(marathons[1]);
+
+    }
+}
--- program.cs
+++ program.cs
@@ -8,4 +8,5 @@
 // Conditionals.ConditionalsRun();
 // Arrays.ArraysRun();
 
-Loops.LoopsRun();
+// Loops.LoopsRun();
+Lists.ListsRun();
