4b7f78267e4326f8f1407ff8fe4e5c74536904d7
Dushimepaulette1 <p.dushime1@alustudent.com>
2026-07-13 11:43:39 +0300

    added the project

--- strings/Program.cs
+++ strings/Program.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 
 namespace LibraryManagementSystem{
   class Program {
+    Cyber Library Management System
+
+// You are tasked with creating a library management system to manage books in your local library. 
+// You’ll be managing a collection of books where each book has a title and an author. 
+// Users should be able to add new books, search for books by title, and view all available books. 
     static void Main() {
       // Initialize library
       Library library = new Library();
