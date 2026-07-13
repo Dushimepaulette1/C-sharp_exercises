eeed10e6c7fb90b559e6b1e60ad4b16912de8a1a
Dushimepaulette1 <p.dushime1@alustudent.com>
2026-07-13 11:41:56 +0300

    Cyber Library Management System project

--- strings/Program.cs
+++ strings/Program.cs
@@ -1,1 +1,24 @@
-Console.WriteLine("Hello from strings!");
+using System;
+using System.Collections.Generic;
+
+namespace LibraryManagementSystem{
+  class Program {
+    static void Main() {
+      // Initialize library
+      Library library = new Library();
+
+      // Add some sample books
+      Book book1 = new Book("1984", "George Orwell");
+      Book book2 = new Book("To Kill a Mockingbird", "Harper Lee");
+
+      library.Addbook(book1);
+      library.Addbook(book2);
+  
+      // Search for a book
+      
+      library.SearchBooksByTitle("1984");
+      // // Display all books
+      library.DisplayAllBooks();
+    }
+  }
+}
