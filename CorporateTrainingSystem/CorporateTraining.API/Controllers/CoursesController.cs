e8cfdeacb4295c6b6828b208c9a100e137dcb259
Dushimepaulette1 <p.dushime1@alustudent.com>
2026-07-29 18:26:51 +0300

    creating a course

--- CorporateTrainingSystem/CorporateTraining.API/Controllers/CoursesController.cs
+++ CorporateTrainingSystem/CorporateTraining.API/Controllers/CoursesController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using CorporateTraining.API.Data;
-
+using CorporateTraining.API.Models;
 namespace CorporateTraining.API.Controllers;
 
 [ApiController]
@@ -18,5 +18,15 @@ public IActionResult GetCourses()
 public IActionResult GetCourse(int id ){
   var course = CourseRepository.GetAllCourses().FirstOrDefault(c => c.Id == id);
   return Ok(course);
+}
+[HttpPost]
+public IActionResult CreateCourse(TrainingCourse newCourse)
+{
+  int nextId = (CourseRepository.GetAllCourses().Max(c => (int?)c.Id) ?? 0) + 1;
+  newCourse.id = nextId;
+  CourseRepository.GetAllCourses().Add(newCourse);
+  return Ok(newCourse);
+  }
+
 }
 }
